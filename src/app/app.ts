9331ef593c66031770793e441c9b8c0840502ad4
FahimMuntashir1 <fahimasdf07@gmail.com>
2023-11-17 09:15:39 +0600

    middleware

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,4 +1,4 @@
-import express, { Request, Response } from 'express';
+import express, { NextFunction, Request, Response } from 'express';
 
 const app = express()
 const port = 3000
@@ -7,11 +7,18 @@ const port = 3000
 app.use(express.json())
 app.use(express.text())
 
-app.get('/', (req:Request, res:Response) => {
-  res.send('Hello Wsssssssorld!')
-})
 
-app.post('/', (req: Request, res: Response) => {
+const logger = (req: Request,res: Response, next: NextFunction) => { 
+  console.log(req.);
+  next();
+}
+
+app.get('/:useId/:subid',logger, (req:Request, res:Response) => {
+  res.send('Hello Wsssssssorld!')
+} 
+)
+
+app.get('/:useId/:subid',logger, (req:Request, res:Response) => {
   console.log(req.body);
   res.send({
     message:'successfully received data'
